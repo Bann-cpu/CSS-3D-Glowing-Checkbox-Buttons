050e41ad00435a5e9a0a6fae1d37a69abdfd6f83
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-02-10 20:53:36 +0100

    Final version

--- style.js
+++ style.js
@@ -1,1 +1,17 @@
-console.log("Tout fonctionne")
+const buttonClass  = document.querySelector(".buttons");
+const labelClass = document.querySelector(".label");
+const inputClass = document.querySelector("input");
+const spanClass = document.querySelector("span");
+const iClass = document.querySelector("i");
+
+const iTransition = [
+    {transform: 'rotate(0deg)', },
+    {transform: 'rotate(180deg)'},
+]
+
+spanClass.addEventListener('click', () => {
+    iClass.animate(iTransition);
+    inputClass.animate(iTransition)
+});
+
+console.log('Le js fonctionne');
